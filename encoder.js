f35faacfe641411a1f72531fb3d5778ba12067ce
John Isutsah <johnisutsa@gmail,com>
2024-01-10 21:02:17 +0300

    Adds batch embedding and saving to db functions

--- encoder.js
+++ encoder.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const fs = require("fs");
 require('@tensorflow/tfjs-node');
 const use = require('@tensorflow-models/universal-sentence-encoder');
-const moviePlots = require("./movie-plots.json");
-const pg = require("pg");
+const moviePlots = require("./movie-plots.json").slice(300);
+const pgp = require("pg-promise")({
+    capSQL: true,
+});
 
 const config = {
     user: process.env.PG_NAME,
@@ -17,24 +19,56 @@ const config = {
     },
 };
 
+const db = pgp(config);
+
+const storeInPG = async (moviePlots) => {
+    console.log("Storing in PG", moviePlots.length);
+    const columns = new pgp.helpers.ColumnSet(['title', 'director', 'plot', 'year', 'wiki', 'cast', 'genre', 'embedding'], { table: 'movie_plots' });
+
+    const values = []
+
+    for (let i = 0; i < moviePlots.length; i++) {
+        values.push({
+            title: moviePlots[i]['Title'],
+            director: moviePlots[i]['Director'],
+            plot: moviePlots[i]['Plot'],
+            year: moviePlots[i]['Release Year'],
+            cast: moviePlots[i]['Cast'],
+            genre: moviePlots[i]['Genre'],
+            wiki: moviePlots[i]['Wiki Page'],
+            embedding: `[${moviePlots[i]['embedding']}]`
+        });
+    }
+
+    const query = pgp.helpers.insert(values, columns);
+
+    await db.none(query)
+        .then(() => {
+            console.log("Success");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
 use.load().then(async model => {
-    // const sampleMoviePlot = moviePlots[0];
-    // console.log(sampleMoviePlot);
+    const batchSize = 1000;
 
-    // const embeddings = await model.embed(sampleMoviePlot["Plot"]);
-    // console.log(embeddings.arraySync());
+    for (let start = 0; start < moviePlots.length; start += batchSize) {
+        const end = Math.min(start + batchSize, moviePlots.length);
 
-    const client = new pg.Client(config);
-    await client.connect();
+        console.log(`Processing items from ${start} to ${end}`);
 
-    try {
-        const pgResponse = await client.query(
-            `SELECT count(*) FROM movie_plots`
-        );
-        console.log('PG RESPONSE', pgResponse.rows);
-    } catch (err) {
-        console.log(err);
-    } finally {
-        await client.end();
+        const movieBatch = moviePlots.slice(start, end);
+        const plotDescriptions = movieBatch.map(movie => movie['Plot']);
+
+        const embeddingsRequest = await model.embed(plotDescriptions);
+        const embeddings = embeddingsRequest.arraySync();
+
+        for (let i = 0; i < movieBatch.length; i++) {
+            movieBatch[i].embedding = embeddings[i];
+        }
+
+        await storeInPG(movieBatch);
     }
 });
