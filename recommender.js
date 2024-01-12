23cd5e466dab4799b1a4e680543aead75235f46e
John Isutsah <johnisutsa@gmail,com>
2024-01-12 09:51:24 +0300

    Adds GET endpoint to get movie recommendations

--- recommender.js
+++ recommender.js
@@ -12,13 +12,13 @@ const config = {
     database: "defaultdb",
     ssl: {
         rejectUnauthorized: true,
-        ca: fs.readFileSync('./ca.pem').toString(),
+        ca: fs.readFileSync('./ca.pem').toString(), // certificate file to secure your database connection. Place it in the root of your project.
     },
 };
 
-use.load().then(async model => {
-
-    const embeddingsRequest = await model.embed('cute little puppies.');
+const getRecommendations = async (search) => {
+    const model = await use.load();
+    const embeddingsRequest = await model.embed(search);
     const embedding = embeddingsRequest.arraySync()[0];
 
     const client = new pg.Client(config);
@@ -31,9 +31,43 @@ use.load().then(async model => {
         );
 
         console.log(pgResponse.rows);
+        // pgResponse.rows.forEach(row => {
+        //     console.log(row.title);
+        // });
+        return pgResponse.rows;
     } catch (error) {
         console.error(error);
     } finally {
         await client.end();
     }
-});
+}
+
+// use.load().then( async model => {
+
+//     const embeddingsRequest = await model.embed('I enjoy taking long walks along the beach with my dog.');
+//     const embedding = embeddingsRequest.arraySync()[0];
+
+//     const client = new pg.Client(config);
+//     await client.connect();
+//     try {
+//         const pgResponse = await client.query(
+//             `SELECT * FROM movie_plots 
+//             ORDER BY embedding <-> '${JSON.stringify(embedding)}'
+//             LIMIT 5;`
+//         );
+
+//         console.log(pgResponse.rows);
+//         // pgResponse.rows.forEach(row => {
+//         //     console.log(row.title);
+//         // });
+//         return pgResponse.rows;
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         await client.end();
+//     }
+// });
+
+module.exports = {
+    getRecommendations,
+}
